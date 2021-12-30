055309b5cacc7be28adb0ca44a151c689af260d5
Vijay_9524 <omvijay44>
2021-12-30 08:26:46 -0800

    i created sample class

---
+++ src/function_interface/Simple.java
@@ -0,0 +1,11 @@
+package function_interface;
+
+public class Simple {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Test Example");
+
+	}
+
+}
